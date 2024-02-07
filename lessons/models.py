33ae4539ec5b41343a4fda7285174c4cd8830dc0
laurabolwell <laurabolwell@hotmail.com>
2024-02-07 10:08:14 +0000

    Update fields and add time choices to lesson model

--- lessons/models.py
+++ lessons/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 DAY_CHOICES = (
     ("Monday", "Monday"),
@@ -10,18 +11,52 @@ DAY_CHOICES = (
     ("Sunday", "Sunday"),
 )
 
+TIME_CHOICES = (
+    ("9:00am", "9:00am"),
+    ("10:00am", "10:00am"),
+    ("11:00am", "11:00am"),
+    ("12:00pm", "12:00pm"),
+    ("1:00pm", "1:00pm"),
+    ("2:00pm", "2:00pm"),
+    ("3:00pm", "3:00pm"),
+)
 
-class Lessons(models.Model):
-    name = models.CharField(max_length=254),
-    description = models.TextField(),
-    min_age = models.SmallIntegerField(),
-    max_age = models.SmallIntegerField(),
-    class_size = models.SmallIntegerField(),
-    price = models.DecimalField(),
-    day = models.CharField(max_length=15, choices=DAY_CHOICES, default="Saturday")
-    time = models.TimeField()
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=254, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    min_age = models.PositiveIntegerField(
+        default=2,
+        validators=[MinValueValidator(2), MaxValueValidator(10)],
+        null=False, blank=False
+    )
+    max_age = models.PositiveIntegerField(
+        default=10,
+        validators=[MinValueValidator(2), MaxValueValidator(10)],
+        null=False, blank=False
+    )
+    class_size = models.PositiveIntegerField(
+        default=12,
+        validators=[MinValueValidator(4), MaxValueValidator(15)],
+        null=False, blank=False
+    )
+    duration = models.DecimalField(
+        max_digits=4, decimal_places=2,
+        default=1, null=False, blank=False
+    )
+    price = models.DecimalField(
+        max_digits=4, decimal_places=2,
+        default=4.75, null=False, blank=False
+    )
+    day = models.CharField(
+        max_length=15, choices=DAY_CHOICES,
+        default="Saturday", null=False, blank=False
+    )
+    time = models.CharField(
+        max_length=15, choices=TIME_CHOICES,
+        default="12:00pm", null=False, blank=False
+    )
+    image = models.ImageField(null=False, blank=False)
 
     def __str__(self):
         return self.name
--- little_riders_academy/settings.py
+++ little_riders_academy/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+if os.path.exists('env.py'):
+    import env  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sl&iw^ka3je!%i1m6x2jx&s=%ej(a81nu+yww=u@gm77%%$g=k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['8000-laurabolwel-littlerider-fpc0w2i4ki8.ws-eu108.gitpod.io']
 
@@ -42,7 +44,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'home'
+    'home',
+    'lessons',
 ]
 
 MIDDLEWARE = [
