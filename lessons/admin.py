e00a6b68f061cf1c8ffe3bfaa721cfd5163f1354
laurabolwell <laurabolwell@hotmail.com>
2024-02-07 10:21:27 +0000

    Customise lesson admin display

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Lesson
 
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'day',
+        'time',
+        'min_age',
+        'max_age',
+        'class_size',
+        'duration',
+        'price',
+        'image'
+    )
+
+
+
 # Register your models here.
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
