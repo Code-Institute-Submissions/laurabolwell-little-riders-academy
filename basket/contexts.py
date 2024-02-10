a9a85798f09a66f299982ba98827eaebe899ac7a
laurabolwell <laurabolwell@hotmail.com>
2024-02-10 22:30:42 +0000

    Update contexts.py to add lessons to basket

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,8 +1,26 @@
+from django.shortcuts import get_object_or_404
+from lessons.models import Lesson
+
+
 def basket_contents(request):
 
     basket_items = []
     total = 0
     lesson_count = 0
+    basket = request.session.get('basket', {})
+
+    for lesson_id, details in basket.items():
+        lesson = get_object_or_404(Lesson, pk=lesson_id)
+        for date in details:
+            quantity = details[date]
+            total += quantity * lesson.price
+            lesson_count += quantity
+            basket_items.append({
+                'lesson_id': lesson_id,
+                'date': date,
+                'quantity': quantity,
+                'lesson': lesson,
+                })
 
     context = {
         'basket_items': basket_items,
