61620e254241dd64543a5f2001b4a9f5253c1314
laurabolwell <laurabolwell@hotmail.com>
2024-02-14 12:09:05 +0000

    Add checkout form

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,12 +3,14 @@ from django.dispatch import receiver
 
 from .models import BookingItem
 
+
 @receiver(post_save, sender=BookingItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
     Update booking total when a booking items is updated/created
     """
     instance.booking.update_total()
+
 
 @receiver(post_delete, sender=BookingItem)
 def update_on_delete(sender, instance, **kwargs):
