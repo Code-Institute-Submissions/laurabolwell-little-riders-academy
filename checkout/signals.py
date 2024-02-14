c04708f7dd2f9ca95eb053d054bc9d9f5a3e10e7
laurabolwell <laurabolwell@hotmail.com>
2024-02-14 11:47:33 +0000

    Add signals to update total on save and delete

---
+++ checkout/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import BookingItem
+
+@receiver(post_save, sender=BookingItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update booking total when a booking items is updated/created
+    """
+    instance.booking.update_total()
+
+@receiver(post_delete, sender=BookingItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update booking total when a booking item is deleted
+    """
+    instance.booking.update_total()
