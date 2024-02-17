96fcf57c8185a4e20c420fed9b3340f60d5d9f93
laurabolwell <laurabolwell@hotmail.com>
2024-02-17 16:39:20 +0000

    Add profile view and urls

--- little_riders_academy/urls.py
+++ little_riders_academy/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('lessons/', include('lessons.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
