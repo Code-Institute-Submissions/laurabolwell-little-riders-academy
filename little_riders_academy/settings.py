fa516b1cd6826dd50809a3ab229bfc5444e0a613
laurabolwell <laurabolwell@hotmail.com>
2024-02-19 13:30:35 +0000

    Add media context processor to settings.py

--- little_riders_academy/settings.py
+++ little_riders_academy/settings.py
@@ -88,6 +88,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'basket.contexts.basket_contents',
+                'django.template.context_processors.media',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
