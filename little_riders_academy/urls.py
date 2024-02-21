265f3560bcecd4080b7f6afcff1d31c7469ee204
laurabolwell <laurabolwell@hotmail.com>
2024-02-21 20:54:37 +0000

    Add 404 and 500 error pages

--- little_riders_academy/urls.py
+++ little_riders_academy/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404, handler500
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'little_riders_academy.views.handler404'
+handler500 = 'little_riders_academy.views.handler500'
