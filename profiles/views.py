97edec88a2f195d1b89c5829c7a05514325b9f85
laurabolwell <laurabolwell@hotmail.com>
2024-02-21 10:10:46 +0000

    Add login_required decorator to profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def profile(request):
     """ Display the user's profile """
     template = 'profiles/profile.html'
