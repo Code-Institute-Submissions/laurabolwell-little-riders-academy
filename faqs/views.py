589fbf82c7b172681c083048e3c4feb8b0eb3cf3
laurabolwell <laurabolwell@hotmail.com>
2024-02-22 23:24:30 +0000

    Add faqs urls and views

--- faqs/views.py
+++ faqs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+
+def view_faqs(request):
+    """ A view to display all FAQs """
+    questions = Question.objects.all()
+
+    context = {
+        'questions': questions,
+    }
+    
+    return render(request, 'faqs/faqs.html', context)
--- little_riders_academy/urls.py
+++ little_riders_academy/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('faqs', include('faqs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'little_riders_academy.views.handler404'
