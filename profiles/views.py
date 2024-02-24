ddfd97700fa2ac6008cf42d1cafa7691d3719259
laurabolwell <laurabolwell@hotmail.com>
2024-02-24 12:35:10 +0000

    Add profile form and POST method for profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 @login_required
 def profile(request):
     """ Display the user's profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Billing information updated successfully.'
+            )
+        else:
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid.'
+            )
+
+    form = UserProfileForm(instance=profile)
+    bookings = profile.bookings.all()
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'bookings': bookings,
+        'on_profile_page': True,
+    }
 
     return render(request, template, context)
