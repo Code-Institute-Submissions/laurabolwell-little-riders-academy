37b6abe14165bb7e3df2b4d490998a0aa93a5e6c
laurabolwell <laurabolwell@hotmail.com>
2024-02-25 21:42:42 +0000

    Add adjust_basket and remove_item views and urls

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<lesson_id>', views.add_to_basket, name='add_to_basket'),
+    path('add/<lesson_id>/', views.add_to_basket, name='add_to_basket'),
+    path('adjust/<lesson_id>/', views.adjust_basket, name='adjust_basket'),
+    path(
+        'remove/<lesson_id>/', views.remove_from_basket,
+        name='remove_from_basket'
+    ),
 ]
--- basket/views.py
+++ basket/views.py
@@ -32,3 +32,41 @@ def add_to_basket(request, lesson_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, lesson_id):
+    """ Adjust the quantity of the lesson on a given date """
+    print("correct view")
+    lesson = Lesson.objects.get(pk=lesson_id)
+    print("lesson")
+    date = request.POST.get('lesson_date')
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+    print("lesson:", lesson)
+    print("date:", date)
+    print("quantity", quantity)
+    print("basket", basket)
+
+    if quantity > 0:
+        basket[lesson_id][date] = quantity
+        messages.success(request, f'Updated {lesson.name} on {date} quantity to {basket[lesson_id][date]}')
+    else:
+        del basket[lesson_id][date]
+        messages.success(request, f'Removed {lesson.name} on {date} from your basket')
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, lesson_id):
+    """ Remove the lesson from the basket """
+    try:
+        date = request.POST.get('lesson_date')
+        basket = request.session.get('basket', {})
+
+        del basket[lesson_id][date]
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
