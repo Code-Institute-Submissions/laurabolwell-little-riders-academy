709c936be14387c804e5333249bec119af265a88
laurabolwell <laurabolwell@hotmail.com>
2024-02-26 17:59:14 +0000

    Add messages to adjust and remove basket views

--- basket/views.py
+++ basket/views.py
@@ -24,35 +24,37 @@ def add_to_basket(request, lesson_id):
             messages.success(request, f'Updated {lesson.name} on {date} quantity to {basket[lesson_id][date]}')
         else:
             basket[lesson_id][date] = quantity
-            messages.success(request, f'Added {lesson.name} on {date} to your basket')
+            messages.success(
+                request, f'Added {lesson.name} on {date} to your basket'
+            )
     else:
         basket[lesson_id] = {}
         basket[lesson_id][date] = quantity
-        messages.success(request, f'Added {lesson.name} on {date} to your basket')
+        messages.success(
+            request, f'Added {lesson.name} on {date} to your basket'
+        )
 
     request.session['basket'] = basket
     return redirect(redirect_url)
 
 
 def adjust_basket(request, lesson_id):
-    """ Adjust the quantity of the lesson on a given date """
-    print("correct view")
+    """ Adjust the quantity of a lesson on a given date """
     lesson = Lesson.objects.get(pk=lesson_id)
-    print("lesson")
-    date = request.POST.get('lesson_date')
     quantity = int(request.POST.get('quantity'))
+    date = request.POST.get('lesson_date')
     basket = request.session.get('basket', {})
-    print("lesson:", lesson)
-    print("date:", date)
-    print("quantity", quantity)
-    print("basket", basket)
 
     if quantity > 0:
         basket[lesson_id][date] = quantity
         messages.success(request, f'Updated {lesson.name} on {date} quantity to {basket[lesson_id][date]}')
     else:
         del basket[lesson_id][date]
-        messages.success(request, f'Removed {lesson.name} on {date} from your basket')
+        if not basket[lesson_id]:
+            del basket[lesson_id]
+        messages.success(
+            request, f'Removed {lesson.name} on {date} from your basket'
+        )
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -60,13 +62,18 @@ def adjust_basket(request, lesson_id):
 
 def remove_from_basket(request, lesson_id):
     """ Remove the lesson from the basket """
+    lesson = Lesson.objects.get(pk=lesson_id)
     try:
         date = request.POST.get('lesson_date')
         basket = request.session.get('basket', {})
 
         del basket[lesson_id][date]
+        messages.success(
+            request, f'Removed {lesson.name} on {date} from your basket'
+        )
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
