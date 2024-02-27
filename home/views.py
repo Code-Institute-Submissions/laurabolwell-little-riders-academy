db5e8618dae45b12533b43e3f90d70e6217b72da
laurabolwell <laurabolwell@hotmail.com>
2024-02-27 21:50:46 +0000

    Import testimonial model to display on home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from testimonials.models import Testimonial
+
 
 def index(request):
     """ A view to return the index page """
+    testimonials = Testimonial.objects.all()
 
-    return render(request, 'home/index.html')
+    context = {
+        'testimonials': testimonials,
+    }
+    template = 'home/index.html'
+
+    return render(request, template, context)
