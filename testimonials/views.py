1cd4324ca6ab81b5c65a88052e3f86d6cceccb45
laurabolwell <laurabolwell@hotmail.com>
2024-02-27 21:48:59 +0000

    Add POST method for leave_testimonial view

--- testimonials/views.py
+++ testimonials/views.py
@@ -7,6 +7,21 @@ from .forms import TestimonialForm
 
 def leave_testimonial(request):
     """ A view to return the testimonial form """
+    if request.method == "POST":
+        testimonial_form = TestimonialForm(request.POST)
+        if testimonial_form.is_valid():
+            testimonial_form.save()
+            messages.success(
+                request,
+                'Testimonial saved, thank you!'
+            )
+            return redirect(reverse('lessons'))
+        else:
+            messages.error(
+                request,
+                'Cannot save testimonial. Please ensure the form is valid.'
+            )
+
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
         bookings = profile.bookings.all()
