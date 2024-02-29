63f6b24a1c3c692967c75605655e88f670cc72d5
laurabolwell <laurabolwell@hotmail.com>
2024-02-29 12:18:29 +0000

    Update webhook handler to save profile info

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 from .models import Booking, BookingItem
 from lessons.models import Lesson
+from profiles.models import UserProfile
 
 import json
 import time
@@ -66,6 +67,21 @@ class StripeWH_Handler:
         for field, value in billing_details.address.items():
             if value == "":
                 billing_details.address[field] = None
+
+        # Update profile information if save_info was selected
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = billing_details.phone
+                profile.default_country = billing_details.address.country
+                profile.default_postcode = billing_details.address.postal_code
+                profile.default_town_or_city = billing_details.address.city
+                profile.default_street_address1 = billing_details.address.line1
+                profile.default_street_address2 = billing_details.address.line2
+                profile.default_county = billing_details.address.state
+                profile.save()
 
         booking_exists = False
         attempt = 1
