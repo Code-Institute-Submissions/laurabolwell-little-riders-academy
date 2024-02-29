fec9cefb9cdd5f26e128a84ed53a1304aa1ecea2
laurabolwell <laurabolwell@hotmail.com>
2024-02-29 12:56:50 +0000

    Fix pep8 error

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little_riders_academy.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'little_riders_academy.settings'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
